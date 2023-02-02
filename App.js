d32061c2f148e2963bcbe887b7bcf6f7dc928a6d
Warren <93488129+WarrenRT@users.noreply.github.com>
2023-02-01 23:09:27 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+import ATMproject from './atm';
+
+
+function App() {
+  return (
+    <>
+
+<ATMproject/>
+
+</>
+
+  );
+}
+
+export default App;
